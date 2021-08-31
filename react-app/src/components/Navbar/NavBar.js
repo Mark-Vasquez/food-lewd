dcbb23c4b9b6ece415047e60908f7b0659eea1c2
Mark Vasquez <markjasonvasquez1@gmail.com>
2021-08-30 20:43:33 -0700

    Login Functionality user story complete

--- react-app/src/components/Navbar/NavBar.js
+++ react-app/src/components/Navbar/NavBar.js
@@ -6,8 +6,9 @@ import { useSelector } from "react-redux";
 import homeLogo from "../../assets/images/icons8-home-48.png";
 
 const NavBar = () => {
-	const user_pic = useSelector((state) => state?.session.user.profile_img);
-	console.log("pikachu", user_pic);
+	const userPic = useSelector((state) => state?.session.user.profile_img);
+	console.log("pikachu", userPic);
+	const userName = useSelector((state) => state?.session?.user?.username);
 
 	return (
 		<nav className={styles.navbar_container}>
@@ -21,25 +22,32 @@ const NavBar = () => {
 				</div>
 				<div className={styles.future_searchbar}></div>
 				<div className={styles.right_icons_wrapper}>
-					<Link to="/" exact={true} activeClassName="active">
-						<img
-							className={styles.homelogo}
-							src={homeLogo}
-							alt="home button"
-						/>
-					</Link>
-				</div>
-				<div>
-					<LogoutButton />
-				</div>
-				<div>
-					<Link to="/profile">
-						<img
-							className={styles.profile_pic}
-							src={user_pic}
-							alt="profile pic"
-						/>
-					</Link>
+					<div>
+						<p className={styles.welcome_message}>
+							Welcome, {userName}!
+						</p>
+					</div>
+					<div>
+						<Link to="/" exact={true}>
+							<img
+								className={styles.homelogo}
+								src={homeLogo}
+								alt="home button"
+							/>
+						</Link>
+					</div>
+					<div>
+						<LogoutButton />
+					</div>
+					<div>
+						<Link to="/profile">
+							<img
+								className={styles.profile_pic}
+								src={userPic}
+								alt="profile pic"
+							/>
+						</Link>
+					</div>
 				</div>
 			</div>
 
