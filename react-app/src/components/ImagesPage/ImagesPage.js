9e587f588cdfe39f449a07a94e64d5b1a9995eed
Mark Vasquez <markjasonvasquez1@gmail.com>
2021-09-01 11:35:06 -0700

    Add mini footer to images page

--- react-app/src/components/ImagesPage/ImagesPage.js
+++ react-app/src/components/ImagesPage/ImagesPage.js
@@ -6,6 +6,8 @@ import { Link } from "react-router-dom";
 import ImageContainer from "./ImageContainer/ImageContainer";
 import Navbar from "../Navbar";
 import styles from "./ImagesPage.module.css";
+import linkedinLogo from "../../assets/images/icons8-linkedin-64.png";
+import githubLogo from "../../assets/images/icons8-github-60.png";
 
 const ImagesPage = () => {
 	const dispatch = useDispatch();
@@ -49,6 +51,64 @@ const ImagesPage = () => {
 									to="/profile">
 									{userName}
 								</Link>
+							</div>
+						</div>
+						{/* Complete users feature after grading */}
+						{/* <div className={styles.user_suggestion_container}>
+							<div className={styles.suggestion_text_container}>
+								<div className={styles.suggestion_text}>
+									Other Food Lovers
+								</div>
+							</div>
+							<div className={styles.users_container}>
+								<div className={styles.indiv_user_container}>
+									<div
+										className={
+											styles.user_icon_suggest
+										}></div>
+									<div
+										className={
+											styles.user_name_suggest
+										}></div>
+								</div>
+							</div>
+						</div> */}
+						<div className={styles.mini_footer_container}>
+							<div className={styles.about_links_container}>
+								<div className={styles.about_links_inner_top}>
+									<div className={styles.github_container}>
+										{" "}
+										<a
+											className={styles.github_text}
+											href="https://github.com/Mark-Vasquez">
+											GitHub
+											<img
+												className={styles.github_logo}
+												src={githubLogo}
+												alt="GitHub"
+											/>
+										</a>
+									</div>
+									<div className={styles.linkedin_container}>
+										<a
+											className={styles.linked_in_text}
+											href="https://www.linkedin.com/in/mark-vasquez-439858212/">
+											Linkedin
+											<img
+												className={
+													styles.linked_in_logo
+												}
+												src={linkedinLogo}
+												alt="LinkedIn"
+											/>
+										</a>
+									</div>
+								</div>
+							</div>
+							<div className={styles.name_container}>
+								<span className={styles.name_text}>
+									Foodlewd developed by Mark Vasquez
+								</span>
 							</div>
 						</div>
 					</div>
